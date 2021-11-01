c87619514bebb17359c4c0b85305326852046b95
Charles Ivia <iviacharles@gmail.com>
2021-11-01 21:44:31 +0300

    initial App.js component with test data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import CardList from "./components/CardList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const testData = [
+  {
+    name: "Dan Abramov",
+    avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4",
+    company: "@facebook",
+  },
+  {
+    name: "Sophie Alpert",
+    avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4",
+    company: "Humu",
+  },
+  {
+    name: "Sebastian Markbåge",
+    avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4",
+    company: "Facebook",
+  },
+];
+export default class App extends Component {
+  state = { profiles: testData };
+
+  addNewProfile = (profileData) => {
+    this.setState((prevState) => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="header">GitHub Cards App</div>
+        <CardList profiles={this.state.profiles} />
+      </div>
+    );
+  }
 }
-
-export default App;
